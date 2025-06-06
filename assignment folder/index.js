ca2ca15df4779db04da7babd8d8ea96ce1490fc1
Jude Okorie <judechukwunedu@gmail.com>
2025-06-06 11:00:15 -0700

    README.md

---
+++ assignment folder/index.js
@@ -0,0 +1,6 @@
+const firstname ="Jude";
+const lastname ="okorie";
+const age =10;
+const school ="surutech";
+
+console.log (`my name is ${firstname} ${ lastname} i am ${age} years old and i study in ${school}`);
---
+++ index.js
@@ -0,0 +1,38 @@
+// let input=window.prompt( ' type your name')
+// console.log( input)
+// let x;// declearation
+// x=10//declearation
+// console.log(x);
+
+
+// var y=30
+// y = 50
+// console.log(y)
+
+
+// const z =100
+// console.log(z)// console
+
+// let fullname = 'Jude'
+// let age = 10
+// let enrolled= true
+// console.log(` name : ${fullname}`)
+// console.log(`age: ${age} years old`)
+// console.log(`isstudent: ${enrolled}`)
+
+
+
+
+const surname = "Okorie";
+const firstname = "Jude ";
+const carrer = "Software Engineer";
+
+
+console.log(`my name is ${surname} ${firstname} and i am a ${carrer}`);
+
+
+document.getElementById("surname").textContent = `surname: ${surname}`
+document.getElementById("firstname").textContent = `firstname: ${firstname}`
+document.getElementById("carrer").textContent = `carrer: ${carrer}`
+document.getElementById("paragraph").textContent = `my name is ${surname} ${firstname}
+and i am a ${carrer}`
