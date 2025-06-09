1b1be9616238985d8e7e3a768c1b12dd8f25d36b
Jude Okorie <judechukwunedu@gmail.com>
2025-06-09 01:50:14 -0700

    PROJECT.com

--- assignment folder/index.js
+++ assignment folder/index.js
@@ -1,7 +1,44 @@
-var firstname ="Jude";
-var lastname ="okorie";
-var age =10;
-var school ="surutech";
+let firstname ="Jude";
+let lastname ="okorie";
+let age =10;
+let school ="surutech";
+let degree = true
+
 
 
 console.log (`my name is ${firstname} ${ lastname} i am ${age} years old and i study in ${school}`);
+console.log ( `isGraduate: ${degree}`)
+
+
+
+
+document.getElementById("firstname").textContent =`firstname: ${firstname}`
+document.getElementById("lastname").textContent =`lastname: ${lastname}`
+document.getElementById("age").textContent =`age: ${age}`
+document.getElementById("school").textContent =`school: ${school}`
+document.getElementById("paragraph").textContent =`my names are ${firstname} ${lastname} i am ${age}
+years old,and i am a student at ${school}`
+
+
+
+const Title = " The Alchemist"
+const Author =" Paulo coelho"
+const Published = " 1988"
+const isAvailable= true
+
+
+document.getElementById("Title").textContent =`Title: ${Title}`
+document.getElementById("Author").textContent =`AUthor: ${Title}`
+document.getElementById("Published").textContent =`Published: ${Title}`
+document.getElementById("Available").textContent =`Available: ${isAvailable}`
+
+
+
+
+var name = prompt("hello friend i am a computer!what is your name");
+alert(name + " is a nice name")
+var secondname = prompt("what is youur secondname?");
+alert ( secondname + " awsome name" )
+var surname = prompt( "what is your surname");
+alert ( surname + " welcome on board");
+ 
